f062f9076b4394c27015189a8d886a33f33479df
soheil babadi <babadishyl@gmail.com>
2024-04-21 12:28:35 +0330

    reduce log.eeror() lines

--- src/main/java/ir/smartpath/senior/controller/AdviceController.java
+++ src/main/java/ir/smartpath/senior/controller/AdviceController.java
@@ -19,34 +19,30 @@ public class AdviceController {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResponseEntity<String> handleException(Exception e) {
-
-        log.error("An error occurred: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("An error occurred: " + e.getMessage());
+        return handleExceptionInternal(e, HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred: " + e.getMessage());
     }
 
     @ExceptionHandler(SocketTimeoutException.class)
     @ResponseBody
     public ResponseEntity<String> handleSocketTimeoutException(SocketTimeoutException e) {
-        log.error("An error occurred: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
-                .body("Request timed out: " + e.getMessage());
+        return handleExceptionInternal(e, HttpStatus.GATEWAY_TIMEOUT, "Request timed out: " + e.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseBody
     public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
-        log.error("An error occurred: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("Request timed out: " + e.getMessage());
+        return handleExceptionInternal(e, HttpStatus.NOT_FOUND, "Resource not found: " + e.getMessage());
     }
 
     @ExceptionHandler(JsonProcessingException.class)
     @ResponseBody
     public ResponseEntity<String> handleJsonProcessingException(JsonProcessingException e) {
-        log.error("An error occurred: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
-                .body("Request timed out: " + e.getMessage());
+        return handleExceptionInternal(e, HttpStatus.NOT_ACCEPTABLE, "Error processing JSON: " + e.getMessage());
     }
-}
 
+    private ResponseEntity<String> handleExceptionInternal(Exception e, HttpStatus status, String message) {
+        log.error("An error occurred: {}", e.getMessage());
+        return ResponseEntity.status(status).body(message);
+    }
+
+}
